968a29d6d28dbd11b2edd0f9cf8a01be980ac32e
JuleeSYX <sayaxang1998@gmail.com>
2023-09-15 23:01:22 +0700

    up

--- index.ts
+++ index.ts
@@ -5,15 +5,15 @@ import compression from 'compression';
 import cors from 'cors';
 import http from 'http';
 import mongoose from 'mongoose';
-const errorHandler = require('./middleware/error-handler');
-const notFound = require('./middleware/not-found');
-const api = require('./controllers/index');
-const srartApi = require('./controllers/login');
+const errorHandler = require('./src/middleware/error-handler');
+const notFound = require('./src/middleware/not-found');
+const api = require('./src/controllers/index');
+const srartApi = require('./src/controllers/login');
 const passport = require('passport');
 const app = express();
 const port = process.env.APP_PORT || 8080;
 require("dotenv").config();
-require("./auth/passport");
+require("./src/auth/passport");
 
 app.use(
     cors({
@@ -30,8 +30,8 @@ app.use(compression());
 app.use(cookieParser());
 app.use(express.json());
 
-const authMiddleware = (req, res, next) => {
-    passport.authenticate("jwt", { session: false }, (err, user, info) => {
+const authMiddleware = (req:any, res:any, next:any) => {
+    passport.authenticate("jwt", { session: false }, (err:any, user:any, info:any) => {
       if (err) {
         return next(err);
       }
@@ -45,7 +45,7 @@ const authMiddleware = (req, res, next) => {
       next();
     })(req, res, next);
 };
-app.get('/', (req, res) => {
+app.get('/', (req:any, res:any) => {
     res.status(200).json("Welcome to my World");
 });
 app.use('/api', authMiddleware, api);
@@ -61,7 +61,7 @@ mongoose.connect(MONGO_URL).then(() => {
         console.log(`Server is runing on port: ${port}`);
     });
 }).catch((err) => console.log(err));
-mongoose.connection.on('error', (error) => console.log(error));
+mongoose.connection.on('error', (error:any) => console.log(error));
 
 app.use(errorHandler);
 app.use(notFound);
